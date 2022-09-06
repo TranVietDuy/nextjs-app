987fb1e08a92f86006e12b914a00e43c89ed3585
Duy Tran <duytran840@learnit.fi>
2022-09-06 23:06:41 +0300

    Change tittle

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
 			</Head>
 
 			<main className={styles.main}>
-				<h1 className={styles.title}>Welcome to Duy's website!</h1>
+				<h1 className={styles.title}>Welcome to website build with NextJs!</h1>
 
 				<p className={styles.description}>
 					Get started by editing{' '}
